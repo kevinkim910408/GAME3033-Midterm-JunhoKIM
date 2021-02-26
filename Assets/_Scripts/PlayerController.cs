6f7f79bb501c5e24a05ea3223d268a04d0a04ab5
kvv(Kevin Junho Kim) <59503331+kevinkim910408@users.noreply.github.com>
2021-02-26 03:26:00 -0500

    Jump
    Jump

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -9,23 +9,67 @@ public class PlayerController : MonoBehaviour
     Vector3 moveVector = Vector3.zero;
 
     [SerializeField] float moveSpeed = 10.0f;
+    [SerializeField] float jumpForce = 5.0f;
 
     // components
     Animator animator;
+    Rigidbody _rigidbody;
+    CapsuleCollider capsuleCollider;
 
     readonly int MovementXHash = Animator.StringToHash("MoveX");
     readonly int MovementZHash = Animator.StringToHash("MoveZ");
+    readonly int IsJumpingHash = Animator.StringToHash("isJumping");
+
+
+    // boolean
+    bool isJump;
+    bool canJump;
 
     // Start is called before the first frame update
     void Awake()
     {
         animator = GetComponent<Animator>();
+        _rigidbody = GetComponent<Rigidbody>();
+        capsuleCollider = GetComponent<CapsuleCollider>();
+
+        canJump = true;
     }
 
     // Update is called once per frame
     void Update()
     {
         Move();
+
+        if (canJump)
+        {
+            Jump();
+        }
+    }
+
+     void FixedUpdate()
+    {
+        // CapsuleCast to check ground
+        //float distanceToPoints = capsuleCollider.height / 2 - capsuleCollider.radius;
+        //Vector3 point1 = transform.position + capsuleCollider.center + Vector3.up * distanceToPoints;
+        //Vector3 point2 = transform.position + capsuleCollider.center - Vector3.up * distanceToPoints;
+        //Vector3 dir = Vector3.down;
+        //float castDistance = 0.1f;
+        //float radius = capsuleCollider.radius;
+
+        //// 0.1f - distance between capsule and ground
+        //RaycastHit[] hit = Physics.CapsuleCastAll(point1, point2, radius, dir, castDistance, groundLayer);
+
+        //foreach(RaycastHit arr in hit)
+        //{
+        //    Debug.Log(arr.collider.name);
+        //}
+
+        //// hit ground
+        //if (hit.Length != 0)
+        //{
+        //    Jump();
+        //}
+
     }
 
     private void Move()
@@ -37,7 +81,18 @@ public class PlayerController : MonoBehaviour
             transform.Translate(moveVector);
         }
     }
+    public void Jump()
+    {
+        if (!isJump)
+            return;
 
+        _rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        isJump = false;
+
+        // prevent double jump
+        canJump = false;
+
+    }
     public void OnMove(InputValue value)
     {
         Vector2 getMoveValue = value.Get<Vector2>();
@@ -47,6 +102,24 @@ public class PlayerController : MonoBehaviour
 
         animator.SetFloat(MovementXHash, getMoveValue.x);
         animator.SetFloat(MovementZHash, getMoveValue.y);
+    }
+
+    public void OnJump(InputValue value)
+    {
+        isJump = value.isPressed; // button press => isjump = true
+
+
+        animator.SetBool(IsJumpingHash, value.isPressed); // jump anim
+        //Debug.Log(value.Get());
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            canJump = true;
+            animator.SetBool(IsJumpingHash, false);
+        }
     }
 
 }
